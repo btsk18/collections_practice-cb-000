4426dacb0d23682f020c75a066e4088a3c784e02
Brandon Leeds <brandonleeds@gmail.com>
2017-12-06 18:44:25 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,3 +11,11 @@ def sort_array_desc(ints)
   }
   ascending_array
 end
+
+def sort_array_char_count(strings)
+  ascending_array = []
+  ascending_array = strings.sort { |a, b|
+    a.length <=> b.length
+  }
+  ascending_array
+end
