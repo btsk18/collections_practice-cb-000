afb8334bd8769394d05d5342c8b92ea6d622f1d8
Brandon Leeds <brandonleeds@gmail.com>
2017-12-06 18:42:04 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,4 +3,11 @@ def sort_array_asc(ints)
   ascending_array = ints.sort
   ascending_array
 end
-  
+
+def sort_array_desc(ints)
+  ascending_array = []
+  ascending_array = ints.sort { |a, b|
+    b <=> a
+  }
+  ascending_array
+end
