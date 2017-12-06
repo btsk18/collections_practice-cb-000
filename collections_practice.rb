44b769afc57b5c471db3f88b166a50840469ddf3
Brandon Leeds <brandonleeds@gmail.com>
2017-12-06 19:04:21 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -54,11 +54,6 @@ end
 
 def add_s(array)
   array.each_with_index.collect { |s, index|
-    if index != 1
-      s << "s"
-    else
-      s
-    end
+    index != 1 ? s << "s" : s
   }
-  # array
 end
